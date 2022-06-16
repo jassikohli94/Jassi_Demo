775bf944a8bd6aa4b748206295a4c24950e1c2e1
jassi <jassi@ikea.com>
2022-06-15 23:37:06 -0400

    Executable change in Simple Java

--- simple.java
+++ simple.java
@@ -1,6 +1,6 @@
 class simple{
-	public static void main(string args[]){
-		system.out.println("Hello World to DevOps Training");
+	public static void main(String args[]){
+		System.out.println("Hello World to DevOps Training");
 	}
 }
 
